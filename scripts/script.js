d1a197d327a032b4ad6efed7fb39ae8fdb3474cb
Lucas <lucaschristiansson05@gmail.com>
2023-05-06 20:21:11 +0200

    Adding search page

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,52 @@
+const category = {
+    NULL : 0,
+    ENVIRONMENT: 1,
+    FACTORY: 2,
+    ENERGY: 3,
+}
+const article = {
+    name: "", 
+    description: "", 
+    category: category.NULL,
+}
+
 window.onload = (event) => {
     const location = window.location.href;
-    urlSplit = location.split('?');
+    var urlSplit = location.split('?');
 
     if(urlSplit){
-        test = new URLSearchParams(urlSplit[1]);
-        console.log(test.get('search'));
+        var test = new URLSearchParams(urlSplit[1]);
+        const searchWord = test.get('search');;
+
+        var search_content = document.getElementById('search-content');
+        search_content.innerHTML = searchWord;
+
+        const arrayToFilter = [
+            { name: "Fusion", description: "En typ av kärnkraft", category: "renew" },
+            { name: "Fission", description: "En typ av kärnkraft", category: ""  },
+            { name: "Vattenkraft", description: "Vattenkraft är kraftigt vatten", category: ""  },
+            
+        ];
+          
+        const filteredArray = arrayToFilter.filter((object) => {
+            const textToCompare = object.name.toLowerCase() + object.description.toLowerCase();
+            const similarityScore = getSimilarityScore(searchWord.toLowerCase(), textToCompare);
+            return similarityScore > 0.1; // adjust threshold as needed
+        });
+        
+        function getSimilarityScore(searchWord, textToCompare) {
+            const distance = levenshteinDistance(searchWord, textToCompare);
+            const maxLength = Math.max(searchWord.length, textToCompare.length);
+            return 1 - (distance / maxLength); // normalize distance to range [0, 1]
+        }
+        console.log(filteredArray);
+
     }
 };
 
 const search_bar = document.getElementById('search-bar');
 
-search_bar.addEventListener("keydown", function(event) {
+search_bar.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         event.preventDefault();
         search();
@@ -22,52 +58,67 @@ function search(){
     if(!search_bar.value){
         return;
     }
-    const location = window.location.href;
+    console.log(window.location.hostname);
+    
+    const location = window.location.origin + "/html/search.html";
 
-    urlSplit = location.split('?');
-    window.location.href = urlSplit[0] + '?search=' + search_bar.value;
+    window.location.href = location + '?search=' + search_bar.value;
 }
 
-if(readCookie("darkmode") != "true" && readCookie("darkmode") != "false"){
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-theme', "dark");
-        createCookie("darkmode","true","365");
-    }else{
-        createCookie("darkmode","false","365");
-        document.documentElement.setAttribute('data-theme', "light");
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if(readCookie('darkmode') != 'true' && readCookie('darkmode') != 'false'){
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }else if(readCookie('darkmode') == 'true'){
+        eraseCookie('darkmode');
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }else if(readCookie('darkmode') == 'false'){
+        document.documentElement.setAttribute('data-theme', 'light');
     }
-
 }else{
-    if(readCookie("darkmode") == "true"){
-        document.documentElement.setAttribute('data-theme', "dark");
-    }else{
-        document.documentElement.setAttribute('data-theme', "light");
+    if(readCookie('darkmode') != 'true' && readCookie('darkmode') != 'false'){
+        document.documentElement.setAttribute('data-theme', 'light');
+    }else if(readCookie('darkmode') == 'false'){
+        eraseCookie('light');
+        document.documentElement.setAttribute('data-theme', 'light');
+    }else if(readCookie('darkmode') == 'true'){
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
 }
 
 function darkToggle(){
     let theme = document.documentElement.getAttribute('data-theme');
-    if(theme == "dark"){ 
-        createCookie("darkmode", "false" ,"365");
-        document.documentElement.setAttribute('data-theme', "light");
-    }else if(theme == "light"){
-        createCookie("darkmode", "true" ,"365");
-        document.documentElement.setAttribute('data-theme', "dark");
+    if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
+        if(theme == 'dark'){
+            createCookie('darkmode', 'false' ,'365');
+            document.documentElement.setAttribute('data-theme', 'light');
+        }else{
+            eraseCookie('darkmode')
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+    }else{
+        if(theme == 'dark'){
+            eraseCookie('darkmode')
+            document.documentElement.setAttribute('data-theme', 'light');
+        }else{
+            createCookie('darkmode', 'true' ,'365');
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
     }
 }
 
+// Code for creating, reading and erasing cookies
 function createCookie(name,value,days) {
 	if (days) {
 		var date = new Date();
 		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
+		var expires = '; expires='+date.toGMTString();
 	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; SameSite=Strict; Secure; path=/";
+	else var expires = '';
+	document.cookie = name+'='+value+expires+'; SameSite=Strict; Secure; path=/';
 }
 
 function readCookie(name) {
-	var nameEQ = name + "=";
+	var nameEQ = name + '=';
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
@@ -78,5 +129,35 @@ function readCookie(name) {
 }
 
 function eraseCookie(name) {
-	createCookie(name,"",-1);
+	createCookie(name,'',-1);
 }
+
+function levenshteinDistance(s, t) {
+    // create empty matrix of size (s.length + 1) x (t.length + 1)
+    const matrix = Array(s.length + 1).fill(null).map(() => Array(t.length + 1).fill(null));
+    
+    // initialize first row and column to consecutive integers
+    for (let i = 0; i <= s.length; i++) {
+      matrix[i][0] = i;
+    }
+    
+    for (let j = 0; j <= t.length; j++) {
+      matrix[0][j] = j;
+    }
+    
+    // fill in the rest of the matrix using recurrence relation
+    for (let i = 1; i <= s.length; i++) {
+      for (let j = 1; j <= t.length; j++) {
+        const cost = s[i - 1] === t[j - 1] ? 0 : 1;
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j] + 1, // deletion
+          matrix[i][j - 1] + 1, // insertion
+          matrix[i - 1][j - 1] + cost // substitution
+        );
+      }
+    }
+    
+    // return bottom-right element of matrix (distance between s and t)
+    return matrix[s.length][t.length];
+  }
+  
