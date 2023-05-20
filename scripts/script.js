c7e6294e5fbf4ae9d7972858b8ef22c1134fe1e8
luckelc <lucaschristiansson05@gmail.com>
2023-05-21 00:46:49 +0200

    Added all articles and fixed search function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,70 @@
+function toggleSearchPanel(){
+    panel = document.getElementById('search-lower-panel');
+    toggleActive(panel);
+}
+
+function getRandomElements(arr, numElements, excludedElement) {
+  if (numElements > arr.length) {
+    throw new Error("Cannot select more elements than available in the array");
+  }
+  
+  const randomElements = [];
+  const cloneArr = [...arr]; // Create a clone of the array to avoid modifying the original array
+  
+  while (randomElements.length < numElements) {
+    const randomIndex = Math.floor(Math.random() * cloneArr.length);
+    const randomElement = cloneArr[randomIndex];
+    
+    if (randomElement !== excludedElement) {
+      randomElements.push(randomElement);
+      cloneArr.splice(randomIndex, 1); // Remove the selected element from the clone array
+    }
+  }
+  
+  return randomElements;
+}
+
+window.onload = (event) => {
+    fetch('../json/data.json')
+      .then(response => response.json())
+      .then(data => {
+        link = location.pathname;
+        const lastPart = link.substring(link.lastIndexOf("/") + 1);
+        console.log(lastPart);
+        for(let i = 0; i < data.pages.length; i++){
+            if(data.pages[i].href.includes(lastPart)){
+                const selectedElements = getRandomElements(data.pages, 3, data.pages[i]);
+                console.log(selectedElements);
+                var article_parent = document.getElementById('related-article-parent');
+                article_parent.innerHTML = '';
+                for(i = 0; i < selectedElements.length; i++){
+                    article_parent.innerHTML += `
+                    <div class="col-xl-4 col-md-6 col-12 p-4 overflow-hidden">
+                        <a class="related-article" href="${selectedElements[i].href}">
+                            <div class="cover">
+                            <img src="${selectedElements[i].image}" alt="">
+                            </div>
+                            <div class="info p-1 pl-3">
+                            <h3>${selectedElements[i].title}</h3>
+                            <p>${selectedElements[i].description}</p>
+                            </div>
+                        </a>
+                    </div>
+                    `
+                ;
+                }
+                break;
+            }
+        }
+        
+        
+      })
+      .catch(error => {
+        console.error(error);
+    });
+      
+};
+
 function dropdownToggle(){
     document.getElementById('settings-button').classList.toggle('active');
     document.getElementById('settings').classList.toggle('active');
@@ -13,24 +80,75 @@ function removeActive(e){
     e.classList.remove('active');
 }
 
-function toggleHover(e){
-    e.classList.toggle('hover');
-}
-function addHover(e){
-    e.classList.add('hover');
-}
-function removeHover(e){
-    e.classList.remove('hover');
+var isTouchDevice = false;
+
+// Feature detection
+if ('ontouchstart' in window || window.DocumentTouch && document instanceof DocumentTouch) {
+  isTouchDevice = true;
 }
 
+// User agent analysis to exclude Firefox Developer Tools
+if (isTouchDevice && navigator.userAgent.toLowerCase().indexOf('firefox') !== -1 && window.matchMedia) {
+  var mq = window.matchMedia('(pointer: fine)');
+  if (mq.matches) {
+    isTouchDevice = false;
+  }
+}
 
+const lower_nav = document.getElementById('lower-nav');
+const back_top = document.getElementById('back-to-top'); 
+const search_panel = document.getElementById('search-lower-panel');
+var lastScrollPosition = window.pageYOffset;
+window.addEventListener("scroll", function() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollTop > 220){
+        addActive(back_top);
+    }else{
+        removeActive(back_top)
+    }
+
+    if(window.innerWidth < 600 || isTouchDevice){
+        var currentScrollPosition = window.pageYOffset;
+        var threshold = 110; // Adjust this value as needed
+
+        if (scrollTop < threshold) {
+            removeActive(lower_nav);
+            return;
+        }
+  
+        if (currentScrollPosition > lastScrollPosition) {
+          addActive(lower_nav);
+          search_panel.classList.add('hidden');
+          
+        } else if(currentScrollPosition < lastScrollPosition){
+          removeActive(lower_nav);
+          search_panel.classList.remove('hidden');
+        }
+        
+        lastScrollPosition = currentScrollPosition;
+    }
+  
+});
+window.addEventListener("resize", function() {
+    if(window.innerWidth > 600){
+        removeActive(lower_nav);
+    }
+});
+document.getElementById("back-to-top").addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 const search_bar = document.getElementById('search-bar');
+const search_form = document.getElementById('search-form');
 search_bar.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         event.preventDefault();
         search();
     }
+});
+search_bar.addEventListener("blur", function() {
+    removeActive(search_form);
 });
 
 function search(){
@@ -40,9 +158,28 @@ function search(){
     }
 
     var currentURL = window.location.href; // Get the current URL
-    var location = currentURL.substring(0, currentURL.lastIndexOf('/')); // Extract the parent folder URL
+    var location = currentURL.substring(0, currentURL.lastIndexOf('/'));
 
     window.location.href = location + '/search.html?search=' + search_bar.value;
+}
+
+const search_bar_lower = document.getElementById('search-bar-panel-lower');
+search_bar_lower.addEventListener('keydown', function(event) {
+    if (event.code == 'Enter') {
+        event.preventDefault();
+        searchLower();
+    }
+});
+function searchLower(){
+
+    if(!search_bar_lower.value){
+        return;
+    }
+
+    var currentURL = window.location.href; // Get the current URL
+    var location = currentURL.substring(0, currentURL.lastIndexOf('/'));
+
+    window.location.href = location + '/search.html?search=' + search_bar_lower.value;
 }
 
 // dark-/lightmode activator
--- scripts/search.js
+++ scripts/search.js
@@ -1,92 +1,59 @@
-const pagesData = [];
-
-// Search parser
 window.onload = (event) => {
   fetch('../json/data.json')
     .then(response => response.json())
     .then(data => {
-      pagesData.push(data.pages);
-
       const location = window.location.href;
       var urlSplit = location.split('?');
 
       if(urlSplit){
-          var test = new URLSearchParams(urlSplit[1]);
-          const searchWord = test.get('search');
+        const searchWord = new URLSearchParams(urlSplit[1]).get('search');
+        
+        var search_content = document.getElementById('search-content');
+        search_content.innerHTML = 'Results for: "' + searchWord + '"';
 
-          var search_content = document.getElementById('search-content');
-          search_content.innerHTML = searchWord;
+        const options = {
+          includeScore: true,
+          keys: [
+            {
+              name: 'title',
+              weight: 0.3
+            },
+            {
+              name: '',
+              weight: 0.7
+            }
+          ]
+        }
+        
+        // Create a new instance of Fuse
+        const fuse = new Fuse(data.pages, options)
+        
+        // Now search for 'Man'
+        const result = fuse.search(searchWord)
+        console.log(searchWord);
+        console.log(result);
 
-          function getSimilarityScore(searchWord, item) {
-              const nameDistance = levenshteinDistance(searchWord, item.title.toLowerCase());
-              const descriptionDistance = levenshteinDistance(searchWord, item.description.toLowerCase());
-              const searchTermsDistance = levenshteinDistance(searchWord, item.search_terms.toLowerCase());
-              const maxLength = Math.max(searchWord.length, item.description.length, item.title.length, item.search_terms.length);
-              const weightedDistance = (nameDistance * 4) + (searchTermsDistance) + descriptionDistance; // weight name twice as much
-              if (nameDistance === 0 || searchTermsDistance === 0) {
-                  return 0; // exact match
-              }
-              return Math.abs(1 - (weightedDistance / maxLength)); // normalize distance to range [0, 1]
-          }
+        const search_parent = document.getElementById('search-result');
+        search_parent.innerHTML = '';
 
-          const pages = pagesData[0].filter((object) => {
-              const similarityScore = getSimilarityScore(searchWord.toLowerCase(), object);
-              console.log(similarityScore);
-              return similarityScore < 0.25; // adjust threshold as needed
-          }).map((object) => {
-              object.similarityScore = getSimilarityScore(searchWord.toLowerCase(), object);
-              return object;
-          }).sort((a, b) => a.similarityScore - b.similarityScore);
-          
-          const search_parent = document.getElementById('search-result');
-          search_parent.innerHTML = '';
-
-          for(i = 0; i < pages.length; i++){
-              search_parent.innerHTML += `
-              <div class="col-xl-4 col-md-6 col-12">
-                  <a href="${pages[i].href}" class="d-block overflow-hidden">
-                      <img src="${pages[i].image}" alt="" width="100%">
-                      <h2>${pages[i].title}</h2>
-                      <p>${pages[i].description}</p>
-                  </a>
-              </div>`
-          ;
-          }
-          console.log(pages);
+        for(i = 0; i < result.length; i++){
+          search_parent.innerHTML += `
+          <div class="col-xl-4 col-md-6 col-12">
+              <a href="${result[i].item.href}" class="d-block overflow-hidden article-page">
+                  <img src="${result[i].item.image}" alt="" width="100%">
+                  <div class="info">
+                    <h2>${result[i].item.title}</h2>
+                    <p>${result[i].item.description}</p>
+                  </div>
+              </a>
+          </div>`
+      ;
       }
+      }
+      
     })
     .catch(error => {
       console.error(error);
   });
     
 };
-
-// A function that calculates a strings closeness to another string, 0 closest and then increasing
-function levenshteinDistance(s, t) {
-    // create empty matrix of size (s.length + 1) x (t.length + 1)
-    const matrix = Array(s.length + 1).fill(null).map(() => Array(t.length + 1).fill(null));
-    
-    // initialize first row and column to consecutive integers
-    for (let i = 0; i <= s.length; i++) {
-      matrix[i][0] = i;
-    }
-    
-    for (let j = 0; j <= t.length; j++) {
-      matrix[0][j] = j;
-    }
-    
-    // fill in the rest of the matrix using recurrence relation
-    for (let i = 1; i <= s.length; i++) {
-      for (let j = 1; j <= t.length; j++) {
-        const cost = s[i - 1] === t[j - 1] ? 0 : 1;
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j] + 1, // deletion
-          matrix[i][j - 1] + 1, // insertion
-          matrix[i - 1][j - 1] + cost // substitution
-        );
-      }
-    }
-    
-    // return bottom-right element of matrix (distance between s and t)
-    return matrix[s.length][t.length];
-}
