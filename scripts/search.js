7dcc74159be0f67214096fc28ae8a38d7dfdb9fe
Lucas <lucaschristiansson05@gmail.com>
2023-05-23 16:40:26 +0200

    Added link to about us page

--- scripts/search.js
+++ scripts/search.js
@@ -7,9 +7,6 @@ window.onload = (event) => {
 
       if(urlSplit){
         const searchWord = new URLSearchParams(urlSplit[1]).get('search');
-        
-        var search_content = document.getElementById('search-content');
-        search_content.innerHTML = 'Results for: "' + searchWord + '"';
 
         const options = {
           includeScore: true,
@@ -17,24 +14,39 @@ window.onload = (event) => {
             {
               name: 'title',
               weight: 0.3
-            },
-            {
-              name: '',
-              weight: 0.7
             }
           ]
         }
         
-        // Create a new instance of Fuse
         const fuse = new Fuse(data.pages, options)
         
-        // Now search for 'Man'
         const result = fuse.search(searchWord)
-        console.log(searchWord);
-        console.log(result);
-
+        
+        var search_content = document.getElementById('search-content');
         const search_parent = document.getElementById('search-result');
         search_parent.innerHTML = '';
+
+        if(result.length === 0){
+          
+          search_content.innerHTML = 'No results found for: "' + searchWord + '"';
+          search_content.insertAdjacentHTML("afterend", '<h2 style="font-size:26px;margin-top:1.5em; margin-bottom: .5em;">Check out these other articles:</h2>');
+          for(i = 0; i < 4; i++){
+            search_parent.innerHTML += `
+            <div class="col-xl-4 col-md-6 col-12 pb-3">
+                <a href="${data.pages[i].href}" class="d-block overflow-hidden article-page">
+                    <img src="${data.pages[i].image}" alt="" width="100%">
+                    <div class="info">
+                      <h2>${data.pages[i].title}</h2>
+                      <p>${data.pages[i].description}</p>
+                    </div>
+                </a>
+            </div>`;
+          }
+          return;
+        }
+        
+        search_content.innerHTML = 'Results for: "' + searchWord + '"';
+
 
         for(i = 0; i < result.length; i++){
           search_parent.innerHTML += `
@@ -46,9 +58,8 @@ window.onload = (event) => {
                     <p>${result[i].item.description}</p>
                   </div>
               </a>
-          </div>`
-      ;
-      }
+          </div>`;
+        }
       }
       
     })
